190bb424d5c4c9e7aa39864a17737f7cc4b414b6
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-01 16:37:37 -0500

    added primative versions of header, Nav and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import heroImage from '../assets/chevron.jpg'
+
+function Header () {
+    return (
+        <header>
+            <h1 className='text-center mt-4'>Alex's Portfolio</h1>
+        </header>
+    )
+}
+
+export default Header
