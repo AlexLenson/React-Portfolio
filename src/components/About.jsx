6a6b28d797f3a848da20de601d9a0b373de77a9b
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-13 03:37:27 -0500

    adjusted project images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,15 @@
 // import profile_pic from '../assets/bootcamp_profile_picture.jpg'
 import profile_pic from '../assets/bowtie-cat.png'
-function About(){
-    return(
+function About() {
+    return (
         <div className='about card'>
             <h1 className='text-center'>About Me</h1>
             <div className="card-body  d-flex justify-content-center">
                 <img src={profile_pic} alt="" />
-                <p>Hello and welcome to my Portfolio page! My name is Alex Lenson, and I am an aspiring web devleoper. On this page you can find my current projects, as well as future projects coming soon. These projects serve as a learning tool to help develop my skills in various web development technologies.</p>
-                <p>I have worked in the aerospace engineering field as a structural engineer for 5 years and I'm looking to make a career change. I joined the UCF coding bootcamp because I would like to become a professional web developer and pursue my interest in programming! These projects will provide an opportunity to practice the skills that I am learning in the bootcamp, while creating something new.</p>
+                <div>
+                    <p>Hello and welcome to my Portfolio page! My name is Alex Lenson, and I am an aspiring web devleoper. Here you can find my current projects, as well as future projects coming soon. These projects serve as a learning tool to help develop my skills in various web development technologies.</p>
+                    <p>I have worked in the aerospace engineering field as a structural engineer for 5 years and I'm looking to make a career change. I joined the UCF coding bootcamp because I would like to become a professional web developer and pursue my interest in programming! These projects will provide an opportunity to practice the skills that I am learning in the bootcamp, while creating something new.</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,12 +2,16 @@
 function Project(props){
     return (
 
-    <div className="card" style={{ width: "30vw", height: "35vh"}}>
-            <img src={props.image} alt="" style={{ width: "29vw", height: "15vh" }} />
+    <div className="card" style={{ width: "30vw", height: "41vh"}}>
+            <img src={props.image} alt="" style={{ width: "29vw", height: "29vh" }} />
         <div className="card-body">
             <h3>{props.title}</h3>
-            <a href={props.link}>Deployed Link</a>
-            <a href={props.repoLink}>Github Repo</a>
+            <div>
+                <a href={props.link}>Deployed Link</a>
+            </div>
+            <div>
+                <a href={props.repoLink}>Github Repo</a>
+            </div>
         </div>
     </div>
     
