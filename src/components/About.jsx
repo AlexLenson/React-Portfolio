0fff86d3a11852db05f364cc38dd1e81f50a393d
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-13 03:16:39 -0500

    added about me information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,8 @@ function About(){
             <h1 className='text-center'>About Me</h1>
             <div className="card-body  d-flex justify-content-center">
                 <img src={profile_pic} alt="" />
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quia, rem odio repellat quis praesentium. Adipisci atque maxime reprehenderit ad enim aperiam id culpa odit dolores. Magni animi nostrum similique.</p>
+                <p>Hello and welcome to my Portfolio page! My name is Alex Lenson, and I am an aspiring web devleoper. On this page you can find my current projects, as well as future projects coming soon. These projects serve as a learning tool to help develop my skills in various web development technologies.</p>
+                <p>I have worked in the aerospace engineering field as a structural engineer for 5 years and I'm looking to make a career change. I joined the UCF coding bootcamp because I would like to become a professional web developer and pursue my interest in programming! These projects will provide an opportunity to practice the skills that I am learning in the bootcamp, while creating something new.</p>
             </div>
         </div>
     )
