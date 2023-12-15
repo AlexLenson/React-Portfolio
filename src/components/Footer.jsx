98d6d9d90f036d5c054eaec61c5e4b9620410649
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-15 02:09:59 -0500

    footer minor spacing adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { Linkedin } from "react-bootstrap-icons"
 function Footer () {
     return(
     <footer className="sticky-bottom">
-        <div className="d-flex justify-content-center gap-3"> 
+        <div className="d-flex justify-content-center gap-4"> 
             <a href="https://github.com/AlexLenson"><Github size={25} color="black"/></a>
-                <a href="https://www.linkedin.com/in/mark-lenson/"><Linkedin size={25} color="black"/></a>
+            <a href="https://www.linkedin.com/in/mark-lenson/"><Linkedin size={25} color="black"/></a>
         </div>
     </footer>
     )
